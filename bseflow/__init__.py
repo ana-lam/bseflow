1be92563b947f226eb47e14380017706d835a431
ana-lam <anakristinalam@gmail.com>
2025-04-17 12:17:35 -0400

    hope im doing this right

--- bseflow/__init__.py
+++ bseflow/__init__.py
@@ -1,16 +1,19 @@
-from .calculate_rates import calculate_rates
-from .file_processing import process_h5_file, load_input_data
-from .formation_channels import get_formation_channels
+from .calculate_rates import BSESimulation
+from .formation_channels import identify_formation_channels
 
-from .plotting.sankey import generate_sankey_plot
-from .plotting.bootstrap import bootstrap_plot
+from .plotting.sankey import plot_sankey
+from .plotting.bootstrap import bootstrapped_2d_kde, bootstrapped_ecdf, bootstrapped_kde
+from .plotting.plotting_code import survival_plot, factors_plot, plot_model_rates
 
 __version__ = "0.1.0"
 __all__ = [
-    "calculate_rates",
-    "process_h5_file",
-    "load_input_data",
-    "get_formation_channels",
-    "generate_sankey_plot",
-    "bootstrap_plot",
+    "BSESimulation",
+    "identify_formation_channels",
+    "plot_sankey",
+    "bootstrapped_2d_kde",
+    "bootstrapped_ecdf",
+    "bootstrapped_kde",
+    "survival_plot",
+    "factors_plot",
+    "plot_model_rates"
 ]

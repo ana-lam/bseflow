8de1d2a0f9ce241280c599a4cd51d6fb0f894f3b
ana-lam <anakristinalam@gmail.com>
2025-05-27 23:20:31 -0400

    some edits

--- bseflow/plotting/sankey.py
+++ bseflow/plotting/sankey.py
@@ -67,15 +67,15 @@ def sankey_data_transform(df, CEE=False, formation_channel=False, custom_path=No
     "StellarMergerBeforeMT" : "1",
     "WDBeforeMT" : "2",
     "MT1" : "3",
-    "StellarMerger1" : "4",
+    "StellarMerger1" : "1",
     "WD1": "5",
     "SN1" : "6",
     "Disbound1" : "7",
     "MT2" : "8",
-    "StellarMerger2": "9",
+    "StellarMerger2": "1",
     "WD2": "10",
     "SN2" : "11",
-    "Disbound2" : "12",
+    "Disbound2" : "7",
     "DCO" : "13",
     "Merges" : "14",
     "NoMerger" : "15"
@@ -87,55 +87,75 @@ def sankey_data_transform(df, CEE=False, formation_channel=False, custom_path=No
     "WDBeforeMT" : "2",
     "CE1" : "3",
     "SMT1" : "4",
-    "StellarMerger1" : "5",
+    "StellarMerger1" : "1",
     "WD1": "6",
     "SN1" : "7",
     "Disbound1" : "8",
     "CE2" : "9",
     "SMT2" : "10",
-    "StellarMerger2": "11",
+    "StellarMerger2": "1",
     "WD2": "12",
     "SN2" : "13",
-    "Disbound2" : "14",
+    "Disbound2" : "8",
     "DCO" : "15",
     "Merges" : "16",
     "NoMerger" : "17"
     }
 
     fc_node_mapping = {
+        "MT1": "0",
         "SN1" : "1",
         "StellarMerger1" : "2",
         "Disbound1" : "3",
         "MT2" : "4",
         "MT2other" : "5",
-        "StellarMerger2" : "6",
+        "StellarMerger2" : "2",
         "SN2": "7",
-        "Merges": "8",
-        "NoMerger": "9"
+        "Disbound2": "3",
+        "DCO" : "8",
+        "Merges": "9",
+        "NoMerger": "10"
     }
 
     df['source'] = ""
     df['target'] = ""
 
     for index, row in df.iterrows():
-        if index == 'MR' or index == 'WD' or index == 'PISN':
-            continue
-        elif index == "ZAMS":
-            df.loc[index, 'source'] = "0"
-            df.loc[index, 'target'] = "0"
+        if formation_channel:
+            if index =='MR' or index == 'WD' or index == "PISN" or index == "ZAMS" \
+                or index == "ZAMS_StellarMergerBeforeMT" or index == "ZAMS_MT1other" or index =='ZAMS_SN1':
+                continue
+            elif index == 'ZAMS_MT1':
+                df.loc[index, 'source'] = "0"
+                df.loc[index, 'target'] = "0"
+            else:
+                source_key = index.split("_")[0]
+                target_key = index.split("_")[1]
+
+                df.loc[index, 'source'] = fc_node_mapping[source_key]
+                df.loc[index, 'target'] = fc_node_mapping[target_key]
         else:
-            source_key = index.split("_")[0]
-            target_key = index.split("_")[1]
-            
-            df.loc[index, 'source'] = CE_node_mapping[source_key] if CEE is True else node_mapping[source_key]
-            df.loc[index, 'target'] = CE_node_mapping[target_key] if CEE is True else node_mapping[target_key]
+            if index == 'MR' or index == 'WD' or index == 'PISN':
+                continue
+            elif index == "ZAMS":
+                df.loc[index, 'source'] = "0"
+                df.loc[index, 'target'] = "0"
+            else:
+                source_key = index.split("_")[0]
+                target_key = index.split("_")[1]
+                
+                df.loc[index, 'source'] = CE_node_mapping[source_key] if CEE is True else node_mapping[source_key]
+                df.loc[index, 'target'] = CE_node_mapping[target_key] if CEE is True else node_mapping[target_key]
 
     trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger', 'MR', 'PISN']
     
     if formation_channel:
-        trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger', 'MR', 'PISN', 'other']
-
-    trash = [node_mapping[key] for key in node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
+        trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger', 'other']
+        trash = [fc_node_mapping[key] for key in fc_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
+    elif CEE:
+        trash = [CE_node_mapping[key] for key in CE_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
+    else:
+        trash = [node_mapping[key] for key in node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
 
     if CEE:
         trash = [CE_node_mapping[key] for key in CE_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
@@ -149,44 +169,49 @@ def sankey_data_transform(df, CEE=False, formation_channel=False, custom_path=No
 
     if formation_channel:
          df.rename(index={'ZAMS_SN1': 'ZAMS_SN1other'}, inplace=True)
-         df.loc[df.index.str.contains("ZAMS_SN1other"), 'color'] = "rgba(115, 115, 115, 0.4)"
          df.rename(index={'SN1_SN2': 'SN1_SN2other'}, inplace=True)
-         df.loc[df.index.str.contains("SN1_SN2other"), 'color'] = "rgba(115, 115, 115, 0.4)"
+         df.loc['SN1_SN2other', 'target'] = 6
+         df.loc[df.index.str.contains("other"), 'color'] = "rgba(220, 85, 0, 0.4)"
     df['values'] = df[df.columns[0]] * 100
 
-    if formation_channel:
-        df.loc[df.index.str.contains("MT1_SN1"), 'target'] = "6"
-        df.loc[df.index.str.contains("SN1_Disbound1"), 'source'] = "6"
-        df.loc[df.index.str.contains("SN1_Disbound1"), 'target'] = "7"
-        df.loc[df.index.str.contains("SN1_MT2"), 'source'] = "6"
-        df.loc[df.index.str.contains("SN1_MT2"), 'target'] = "8"
-        df.loc[df.index.str.contains("MT2_StellarMerger2"), 'source'] = "8"
-        df.loc[df.index.str.contains("MT2_StellarMerger2"), 'target'] = "9"
-        df.loc[df.index.str.contains("MT2_SN2"), 'source'] = "9"
-        df.loc[df.index.str.contains("MT2_SN2"), 'target'] = "10"
-        df.loc[df.index.str.contains("SN1_SN2other"), 'source'] = "6"
-        df.loc[df.index.str.contains("SN1_SN2other"), 'target'] = "15"
+        # df['rates'] = df['rates']/(df['rates'][0].value())
+        # df.loc[df.index.str.contains("MT1_SN1"), 'target'] = "6"
+        # df.loc[df.index.str.contains("SN1_Disbound1"), 'source'] = "6"
+        # df.loc[df.index.str.contains("SN1_Disbound1"), 'target'] = "7"
+        # df.loc[df.index.str.contains("SN1_MT2"), 'source'] = "6"
+        # df.loc[df.index.str.contains("SN1_MT2"), 'target'] = "8"
+        # df.loc[df.index.str.contains("MT2_StellarMerger2"), 'source'] = "8"
+        # df.loc[df.index.str.contains("MT2_StellarMerger2"), 'target'] = "9"
+        # df.loc[df.index.str.contains("MT2_SN2"), 'source'] = "9"
+        # df.loc[df.index.str.contains("MT2_SN2"), 'target'] = "10"
+        # df.loc[df.index.str.contains("SN1_SN2other"), 'source'] = "6"
+        # df.loc[df.index.str.contains("SN1_SN2other"), 'target'] = "15"
 
     df = df.iloc[4: , :]
 
+    if formation_channel:
+        df = df[~df.index.str.contains('ZAMS')]
+        renormalize_value = df.iloc[1]['values'] if df.iloc[0]['values'] == 0 else df.iloc[0]['values']
+        df['values'] = df['values']/renormalize_value * 100
+
     return df
 
-def plot_sankey(df, title="", CEE=False, formation_channel=False, save_path=None, custom_trash=None):
+def plot_sankey(df, title="", CEE=False, formation_channel=False, save_path=None, custom_path_labels=None, custom_trash=None):
 
     node_mapping = {
     "ZAMS" : "0",
     "StellarMergerBeforeMT" : "1",
     "WDBeforeMT" : "2",
     "MT1" : "3",
-    "StellarMerger1" : "4",
+    "StellarMerger1" : "1",
     "WD1": "5",
     "SN1" : "6",
     "Disbound1" : "7",
     "MT2" : "8",
-    "StellarMerger2": "9",
+    "StellarMerger2": "1",
     "WD2": "10",
     "SN2" : "11",
-    "Disbound2" : "12",
+    "Disbound2" : "7",
     "DCO" : "13",
     "Merges" : "14",
     "NoMerger" : "15"
@@ -198,63 +223,130 @@ def plot_sankey(df, title="", CEE=False, formation_channel=False, save_path=None
         "WDBeforeMT" : "2",
         "CE1" : "3",
         "SMT1" : "4",
-        "StellarMerger1" : "5",
+        "StellarMerger1" : "1",
         "WD1": "6",
         "SN1" : "7",
         "Disbound1" : "8",
         "CE2" : "9",
         "SMT2" : "10",
-        "StellarMerger2": "11",
+        "StellarMerger2": "1",
         "WD2": "12",
         "SN2" : "13",
-        "Disbound2" : "14",
+        "Disbound2" : "8",
         "DCO" : "15",
         "Merges" : "16",
         "NoMerger" : "17"
         }
+    
+    fc_node_mapping = {
+        "MT1": "0",
+        "SN1" : "1",
+        "StellarMerger1" : "2",
+        "Disbound1" : "3",
+        "MT2" : "4",
+        "MT2other" : "5",
+        "SN2other" : "6",
+        "StellarMerger2" : "2",
+        "SN2": "7",
+        "Disbound2": "3",
+        "DCO" : "8",
+        "Merges": "9",
+        "NoMerger": "10"
+    }
 
     better_labels = {
-    "ZAMS" : "ZAMS",
-    "StellarMergerBeforeMT" : "Stellar Merger",
-    "WDBeforeMT" : "White Dwarf",
-    "MT1" : "Mass Transfer",
-    "CE1" : "CE",
-    "SMT1" : "Stable MT",
-    "StellarMerger1" : "Stellar Merger",
-    "WD1": "White Dwarf",
-    "SN1" : "Supernova",
-    "Disbound1" : "Unbound",
-    "MT2" : "Mass Transfer",
-    "CE2" : "CE",
-    "SMT2" : "Stable MT",
-    "StellarMerger2": "Stellar Merger",
-    "WD2": "White Dwarf",
-    "SN2" : "Supernova",
-    "Disbound2" : "Unbound",
-    "DCO" : "DCO Formation",
-    "Merges" : r'Merges $< t_H$',
-    "NoMerger" : r'Merges $> t_H$'
-    }
+        "ZAMS" : "ZAMS",
+        "StellarMergerBeforeMT" : "Stellar Merger",
+        "WDBeforeMT" : "White Dwarf",
+        "MT1" : "Mass Transfer",
+        "CE1" : "CE",
+        "SMT1" : "Stable MT",
+        "StellarMerger1" : "Stellar Merger",
+        "WD1": "White Dwarf",
+        "SN1" : "Supernova",
+        "Disbound1" : "Unbound",
+        "MT2" : "Mass Transfer",
+        "CE2" : "CE",
+        "SMT2" : "Stable MT",
+        "StellarMerger2": "Stellar Merger",
+        "WD2": "White Dwarf",
+        "SN2" : "Supernova",
+        "Disbound2" : "Unbound",
+        "DCO" : "DCO Formation",
+        "Merges" : r'Merges $< t_H$',
+        "NoMerger" : r'Merges $> t_H$'
+        }
+    
+    fc_better_labels = {
+        "MT1": f"{custom_path_labels[0]}",
+        "SN1" : "Supernova",
+        "StellarMerger1" : "Stellar Merger",
+        "Disbound1" : "Unbound",
+        "MT2" : f"{custom_path_labels[1]}",
+        "MT2other" : f"{custom_path_labels[2]}",
+        "SN2other" : "Supernova",
+        "StellarMerger2" : "Stellar Merger",
+        "SN2": "Supernova",
+        "Disbound2": "Unbound",
+        "DCO" : "DCO Formation",
+        "Merges" : r'Merges $< t_H$',
+        "NoMerger" : r'Merges $> t_H$'
+        }
 
     trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger']
     if formation_channel:
-        trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger', 'other']
-
-    trash = [node_mapping[key] for key in node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
-
-    if CEE:
-        trash = [CE_node_mapping[key] for key in CE_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
-
-    labels = [better_labels[key].replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>') for key in node_mapping.keys()]
-    if CEE:
-        labels = [better_labels[key].replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>') for key in CE_node_mapping.keys()]
-    # labels = [key.replace("1", "").replace("2", "").replace("3", "") for key in node_mapping.keys()]
+        trash_substrings = ['StellarMerger', 'WD', 'Disbound', 'NoMerger']
+        trash = [fc_node_mapping[key] for key in fc_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
     
-    node_colors = ["rgba(19, 121, 32, 0.73)" if value not in trash
-               else "rgba(115, 115, 115, 0.4)" for value in node_mapping.values()]
-    if CEE:
+    elif CEE:
+        trash = [CE_node_mapping[key] for key in CE_node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
+    
+    else:
+        trash = [node_mapping[key] for key in node_mapping.keys() if any(map(key.__contains__, trash_substrings))]
+
+    if formation_channel:
+
+        labels = [fc_better_labels[key].replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>') for key in fc_node_mapping.keys()]
+
+        fc_node_mapping_int = {k: int(v) for k, v in fc_node_mapping.items()}
+
+        # Max index
+        max_index = max(fc_node_mapping_int.values())
+        labels = [""] * (max_index + 1)
+
+        # Fill in correct label at correct index
+        for phase, idx in fc_node_mapping_int.items():
+            label_text = fc_better_labels.get(phase, phase)
+            label_text = label_text.replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>')
+            labels[idx] = label_text
+
+    elif CEE:
+        labels = [better_labels[key].replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>') for key in CE_node_mapping.keys()]
+    else:
+        labels = [better_labels[key].replace(r'$< t_H$', '<t<sub>H</sub>').replace(r'$> t_H$', '>t<sub>H</sub>') for key in node_mapping.keys()]
+    
+    if formation_channel:
+        node_colors = ["rgba(19, 121, 32, 0.73)",
+                       "rgba(19, 121, 32, 0.73)",
+                       "rgba(115, 115, 115, 0.4)",
+                       "rgba(115, 115, 115, 0.4)",
+                       "rgba(19, 121, 32, 0.73)",
+                       "rgba(220, 85, 0, 0.4)",
+                       "rgba(220, 85, 0, 0.4)",
+                       "rgba(115, 115, 115, 0.4)",
+                       "rgba(19, 121, 32, 0.73)",
+                       "rgba(19, 121, 32, 0.73)",
+                       "rgba(115, 115, 115, 0.4)",]
+        # node_colors = ["rgba(19, 121, 32, 0.73)" if label not in trash else "rgba(220, 85, 0, 0.4)" if any(map(value.__contains__, "other"))
+        #        else "rgba(115, 115, 115, 0.4)" for label in labels]
+
+    elif CEE:
         node_colors = ["rgba(19, 121, 32, 0.73)" if value not in trash
                else "rgba(115, 115, 115, 0.4)" for value in CE_node_mapping.values()]
+    else:
+        node_colors = ["rgba(19, 121, 32, 0.73)" if value not in trash
+               else "rgba(115, 115, 115, 0.4)" for value in node_mapping.values()]
+
     if custom_trash:
         for phase in custom_trash:  
             phase_index = list(CE_node_mapping.keys()).index(phase)
